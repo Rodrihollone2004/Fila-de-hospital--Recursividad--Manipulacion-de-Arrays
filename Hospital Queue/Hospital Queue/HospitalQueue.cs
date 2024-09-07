63fe530b0fd50cb3929e0ea3f45c1f357141c843
Rodrihollone2004 <rodri.g.fretes@gmail.com>
2024-09-06 23:54:25 -0300

    Hospital Queue

---
+++ Hospital Queue/Hospital Queue/HospitalQueue.cs
@@ -0,0 +1,23 @@
+﻿public class HospitalQueue
+{
+    Random random;
+    int priority = 0;
+    PriorityQueue<string, int> priorityQueue = new PriorityQueue<string, int>();
+
+    public void Queue()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            random = new Random();
+            priority = random.Next(1, 4);
+            string name = $"Pacient: {i} -- Priority: {priority}";
+            priorityQueue.Enqueue(name, priority);
+        }
+
+        while (priorityQueue.Count > 0)
+        {
+            string pacient = priorityQueue.Dequeue();
+            Console.WriteLine(pacient);
+        }
+    }
+}
---
+++ Hospital Queue/Hospital Queue/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        HospitalQueue hospitalQueue = new HospitalQueue();
+        hospitalQueue.Queue();
+    }
+}
